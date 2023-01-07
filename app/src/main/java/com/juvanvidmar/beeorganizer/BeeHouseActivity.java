b1acf8bae5e04f0c569b8d9ff4734f4a71e45201
AV-G <68665243+AV-G@users.noreply.github.com>
2023-01-07 17:42:02 +0100

    Go to new activity on button click. Title changed on activity. Created constraints on home activity.

--- app/src/main/java/com/juvanvidmar/beeorganizer/BeeHouseActivity.java
+++ app/src/main/java/com/juvanvidmar/beeorganizer/BeeHouseActivity.java
@@ -10,5 +10,6 @@ public class BeeHouseActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bee_house);
+        this.setTitle("Čebeljnjak Placeholder");
     }
 }
--- app/src/main/java/com/juvanvidmar/beeorganizer/HomeActivity.java
+++ app/src/main/java/com/juvanvidmar/beeorganizer/HomeActivity.java
@@ -2,6 +2,7 @@ package com.juvanvidmar.beeorganizer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,12 +23,24 @@ Button newbtn;
                 addButton();
             }
         });
+        this.setTitle("Dobrodošel placeholder@placeholder.com");
     }
 
     public void addButton() {
         LinearLayout layout = (LinearLayout) findViewById(R.id.rootlayout);
         newbtn = new MaterialButton(this);
-        newbtn.setText("newButton");
+        newbtn.setText("Čebeljnjak X");
+        newbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToBeeHouseActivity();
+            }
+        });
         layout.addView(newbtn);
+    }
+
+    public void goToBeeHouseActivity() {
+        Intent intent = new Intent(this, BeeHouseActivity.class);
+        startActivity(intent);
     }
 }
