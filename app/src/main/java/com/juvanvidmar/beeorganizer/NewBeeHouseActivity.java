e9ed4f04c21f6062b391a53c728527ee4bbf4964
Žan Juvan <zan.juvan01@gmail.com>
2023-01-12 17:50:09 +0100

    Update new beehive

--- app/src/main/java/com/juvanvidmar/beeorganizer/NewBeeHouseActivity.java
+++ app/src/main/java/com/juvanvidmar/beeorganizer/NewBeeHouseActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -19,6 +20,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.VolleyLog;
 import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -26,12 +28,14 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NewBeeHouseActivity extends AppCompatActivity {
 
     private TextView status;
     private EditText naziv;
-    //private EditText steviloPanjev;
+    private EditText steviloPanjev;
 
     private RequestQueue requestQueue;
     private String url = "https://thebeeorganizer.azurewebsites.net/api/v1/cabeljnjak/";
@@ -45,7 +49,7 @@ public class NewBeeHouseActivity extends AppCompatActivity {
         status = (TextView) findViewById(R.id.TVstatusPostCebeljnjak);
         status.setText("");
         naziv = (EditText) findViewById(R.id.ETnazivCebeljnjaka);
-        //steviloPanjev = (EditText) findViewById(R.id.ETstPanjev);
+        steviloPanjev = (EditText) findViewById(R.id.ETstPanjev);
 
         Button but1 = (Button) findViewById(R.id.butPrekliciCebeljnjak);
         but1.setOnClickListener(new View.OnClickListener() {
@@ -73,54 +77,92 @@ public class NewBeeHouseActivity extends AppCompatActivity {
             SharedPreferences sharedPreferences = getSharedPreferences("user_prefs", MODE_PRIVATE);
             String userId =  sharedPreferences.getString("id", "");
             JSONObject jsonBody = new JSONObject();
-            jsonBody.put("naslov", String.valueOf(naziv.getText()));
-            jsonBody.put("uporabnikId", userId);
+            jsonBody.put("naziv", String.valueOf(naziv.getText()));
+            jsonBody.put("userId", userId);
+            jsonBody.put("num", Integer.parseInt(String.valueOf(steviloPanjev.getText())));
+
+            Uri.Builder builder = Uri.parse(url).buildUpon();
+            builder.appendQueryParameter("naziv", String.valueOf(naziv.getText()));
+            builder.appendQueryParameter("userId", userId);
+            builder.appendQueryParameter("num", String.valueOf(steviloPanjev.getText()));
+            String url = builder.build().toString();
 
             final String mRequestBody = jsonBody.toString();
 
-            status.setText(mRequestBody);
+            status.setText("Processing!");
 
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+
+
+            StringRequest stringRequest = new StringRequest(Request.Method.PUT, url, new Response.Listener<String>() {
                 @Override
                 public void onResponse(String response) {
-                    Log.i("LOG_VOLLEY", response);
+                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                    startActivity(intent);
+                    Log.i("LOG_VOLLEY", response.toString());
                 }
             }, new Response.ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
                     Log.e("LOG_VOLLEY", error.toString());
                 }
-            }
-            ) {
+            }){
                 @Override
-                public String getBodyContentType() {
-                    return "application/json; charset=utf-8";
+                public Map<String, String> getHeaders() throws AuthFailureError {
+                    Map<String, String> headers = new HashMap<>();
+                    headers.put("accept", "text/plain");
+                    return headers;
                 }
-                @Override
-                public byte[] getBody() throws AuthFailureError {
-                    try {
-                        return mRequestBody == null ? null : mRequestBody.getBytes("utf-8");
-                    } catch (UnsupportedEncodingException uee) {
-                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", mRequestBody, "utf-8");
-                        return null;
-                    }
-                }
-                @Override
-                protected Response<String> parseNetworkResponse(NetworkResponse response) {
-                    String responseString = "";
-                    if (response != null) {
-                        responseString = String.valueOf(response.statusCode);
-                        status.setText(responseString);
-                    }
-
-                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
-                }
-
             };
-
             requestQueue.add(stringRequest);
-            Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
-            startActivity(intent);
+
+
+//            StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+//                @Override
+//                public void onResponse(String response) {
+//                    Log.i("LOG_VOLLEY", "response");
+//                }
+//            }, new Response.ErrorListener() {
+//                @Override
+//                public void onErrorResponse(VolleyError error) {
+//                    Log.e("LOG_VOLLEY", error.toString());
+//                }
+//            }
+//            ) {
+//                @Override
+//                public String getBodyContentType() {
+//                    return "application/json; charset=utf-8";
+//                }
+//                @Override
+//                public byte[] getBody() throws AuthFailureError {
+//                    try {
+//                        return mRequestBody == null ? null : mRequestBody.getBytes("utf-8");
+//                    } catch (UnsupportedEncodingException uee) {
+//                        VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", mRequestBody, "utf-8");
+//                        return null;
+//                    }
+//                }
+//                @Override
+//                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+//                    String responseString = "";
+//                    if (response != null) {
+//                        responseString = String.valueOf(response.statusCode);
+//
+//                    }
+//                    if (responseString.equals("201") || responseString.equals("200")) {
+//                        Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+//                        startActivity(intent);
+//                    } else
+//                    {
+//                        status.setText("Sorry! Something gone wrong!");
+//                    }
+//
+//                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+//                }
+//
+//            };
+//
+//            requestQueue.add(stringRequest);
+
         } catch (JSONException e) {
             e.printStackTrace();
         }
